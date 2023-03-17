9069b1957d29325cb0804425318ce8e7cab19ede
Sayem92 <sayemmiha123@gmail.com>
2023-03-17 22:40:41 +0600

    add blog

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Blog from './Blog/Blog';
 import Studies from './Studies/Studies';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
            <Banner></Banner>
            <Studies></Studies>
+           <Blog></Blog>
         </div>
     );
 };
