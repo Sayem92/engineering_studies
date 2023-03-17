7a721d1bcbc5919feac0786fceed86ff756267b4
Sayem92 <sayemmiha123@gmail.com>
2023-03-17 16:32:28 +0600

    add route, layout and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import { router } from "./components/Routes/Routes";
 
 function App() {
   return (
-    <div className="">
-  hello world
-  
+    <div className="max-w-[1440px] mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
