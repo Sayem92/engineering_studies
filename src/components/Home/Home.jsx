2eee016ac11972e4e39ccc821e4a7aea15754604
Sayem92 <sayemmiha123@gmail.com>
2023-03-17 23:26:08 +0600

    add contact

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Blog from './Blog/Blog';
+import Contact from './Contact/Contact';
 import Studies from './Studies/Studies';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
            <Banner></Banner>
            <Studies></Studies>
            <Blog></Blog>
+           <Contact></Contact>
         </div>
     );
 };
--- src/components/Home/Studies/StudiesCard.jsx
+++ src/components/Home/Studies/StudiesCard.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const StudiesCard = ({ data }) => {
   return (
     <div>
-      <div class="max-w-sm overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
-        <img src={data?.image} alt="plant" class="h-64 w-full" />
-        <div class="p-5">
+      <div className="max-w-sm overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
+        <img src={data?.image} alt="plant" className="h-64 w-full" />
+        <div className="p-5">
           <h1 className="text-center font-bold py-2">{data?.name}</h1>
           <div className="flex justify-center mt-5">
-            <button class=" rounded-md bg-indigo-600 px-3 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">
+            <button className=" rounded-md bg-indigo-600 px-3 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">
               Explore
             </button>
           </div>
