fb7696533e4c69384446e09cf9111eb984a5a3a8
Sayem92 <sayemmiha123@gmail.com>
2023-03-18 20:28:13 +0600

    add banner title

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -10,7 +10,9 @@ const Banner = () => {
         <div className="my-20 xl:mt-52 md:w-11/12 lg:w-8/12 xl:w-7/12 text-white">
           <div>
             <h1 className=" text-3xl xl:text-5xl leading-snug font-semibold mb-5">
-              Find the perfect <i>freelance</i> services for your business
+              Welcome to way of productive{" "}
+              <i className="text-purple-600">study </i>
+              save your time
             </h1>
           </div>
         </div>
