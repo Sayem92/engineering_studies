5ea1c1d60a61c9924360c9c1c2d83d9fd74b67d6
Sayem92 <sayemmiha123@gmail.com>
2023-03-18 23:06:39 +0600

    update banner

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
       className={`xl:min-h-screen banner-container px-4 py-5 mx-auto md:px-24 z-0`}
     >
       <div className="grid grid-cols-1 justify-center items-center">
-        <div className="my-20 xl:mt-52 md:w-11/12 lg:w-8/12 xl:w-7/12 text-white">
+        <div className="my-20 xl:mt-52 md:w-11/12 lg:w-8/12 xl:w-7/12 text-green-300">
           <div>
             <h1 className=" text-3xl xl:text-5xl leading-snug font-semibold mb-5">
               Welcome to way of productive{" "}
