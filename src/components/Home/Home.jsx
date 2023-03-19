46c851be2512202658afb27d28e103c08ce87025
Sayem92 <sayemmiha123@gmail.com>
2023-03-19 17:25:42 +0600

    update banner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import AboutUS from './AboutUS/AboutUS';
-import Banner from './Banner/Banner';
-import Blog from './Blog/Blog';
-import Contact from './Contact/Contact';
-import Studies from './Studies/Studies';
+import React from "react";
+import AboutUS from "./AboutUS/AboutUS";
+import Banner from "./Banner/Banner/Banner";
+import Blog from "./Blog/Blog";
+import Contact from "./Contact/Contact";
+import Studies from "./Studies/Studies";
 
 const Home = () => {
-    return (
-        <div>
-           <Banner></Banner>
-           <Studies></Studies>
-           <Blog></Blog>
-           <AboutUS></AboutUS>
-           <Contact></Contact>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <Studies></Studies>
+      <Blog></Blog>
+      <AboutUS></AboutUS>
+      <Contact></Contact>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Home/Studies/Studies.jsx
+++ src/components/Home/Studies/Studies.jsx
@@ -1,41 +1,45 @@
 import React from "react";
 import StudiesCard from "./StudiesCard";
-import cse from '../../../assets/CSE.jpeg';
-import eee from '../../../assets/EEE.jpeg';
-import civil from '../../../assets/CIVIL.jpeg';
+import cse from "../../../assets/CSE.jpeg";
+import eee from "../../../assets/EEE.jpeg";
+import civil from "../../../assets/CIVIL.jpeg";
 
 const Studies = () => {
   const studies = [
     {
       name: "Computer Science and Engineering",
       image: cse,
-      title: "CSE"
+      title: "CSE",
     },
     {
       name: "Electrical and Electronics Engineering",
       image: eee,
-      title: "EEE"
+      title: "EEE",
     },
     {
       name: "Civil Engineering",
       image: civil,
-      title: "CE"
+      title: "CE",
     },
   ];
   return (
-    <div className="py-24 px-10 bg-gradient-to-r from-slate-300 to-slate-100">
-      <div className="text-center">
-        <h1 className="font-bold text-3xl">AVAILABLE <span className="text-purple-600">COURSES</span></h1>
-        <p className="py-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia placeat
-          soluta obcaecati recusandae a odio provident perspiciatis magnam
-          distinctio voluptatem.
-        </p>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
-        {studies?.map((data, i) => (
-          <StudiesCard key={i} data={data}></StudiesCard>
-        ))}
+    <div className="py-24 px-10 ">
+      <div className=" p-10 bg-gradient-to-r from-slate-300 to-slate-100">
+        <div className="text-center">
+          <h1 className="font-bold text-3xl">
+            AVAILABLE <span className="text-purple-600">COURSES</span>
+          </h1>
+          <p className="py-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+            placeat soluta obcaecati recusandae a odio provident perspiciatis
+            magnam distinctio voluptatem.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
+          {studies?.map((data, i) => (
+            <StudiesCard key={i} data={data}></StudiesCard>
+          ))}
+        </div>
       </div>
     </div>
   );
